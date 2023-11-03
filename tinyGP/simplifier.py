f0fa23021a2ddf12b7028117edc1ff2789873d30
LukZawadzki <92645981+LukZawadzki@users.noreply.github.com>
2023-11-03 16:17:08 +0100

    Update simplifier.py to multithreading

--- tinyGP/simplifier.py
+++ tinyGP/simplifier.py
@@ -1,27 +1,41 @@
 import sympy
 import os
+import threading
 
 
 class Simplifier:
 
-    def simplify(self, fileName: str):
-        with open("../tinyGP/gp_output/"+fileName, "rb") as f:
-            try:  # catch OSError in case of a one line file
-                f.seek(-2, os.SEEK_END)
-                while f.read(1) != b'\n':
-                    f.seek(-2, os.SEEK_CUR)
-            except OSError:
-                f.seek(0)
-            last_line = f.readline().decode()
-            last_line = last_line[1:]
+    @staticmethod
+    def simplify(fileName: str):
+        try:
+            with open(fileName, "rb") as f:
+                try:  # catch OSError in case of a one line file
+                    f.seek(-2, os.SEEK_END)
+                    while f.read(1) != b'\n':
+                        f.seek(-2, os.SEEK_CUR)
+                except OSError:
+                    f.seek(0)
+                last_line = f.readline().decode()
+                last_line = last_line[1:]
 
-        print("Simplifying from file: ../tinyGP/gp_output/"+fileName)
+            print("Simplifying from file: "+fileName)
 
-        simplified = sympy.simplify(last_line).__str__()
+            simplified = sympy.simplify(last_line).__str__()
 
-        simplified = "="+simplified.replace("**", "^")
+            simplified = "="+simplified.replace("**", "^")
 
-        with open("../tinyGP/simplified_output/"+fileName, "w") as f:
-            f.write(simplified)
+            with open(fileName, "w") as f:
+                f.write(simplified)
 
-        print(f"Simplified from {len(last_line)} chars to {len(simplified)} chars")
+            print(f"Simplified from {len(last_line)} chars to {len(simplified)} chars")
+        except Exception:
+            print("Couldn't simplify from file "+fileName)
+
+    def run(self, path: str, from_dir: bool):
+        if from_dir is False:
+            self.simplify(path)
+        else:
+            files = os.listdir(path)
+            for fileName in files:
+                x = threading.Thread(target=self.simplify, args=(path+fileName,))
+                x.start()
